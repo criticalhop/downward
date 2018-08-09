0895c3a4733a210552402f9ab3f366f5ff114136
Daniel Gnad <devnull@localhost>
2018-08-09 16:48:16 +0200

    removed (hopefully) unnecessary imports

--- src/translate/subdominization/learn.py
+++ src/translate/subdominization/learn.py
@@ -1,28 +1,11 @@
 #!/usr/bin/python
 
-import numpy as np
-import pandas as pd
-
-import pickle
-
-import sklearn
-
-from sklearn import tree
-from sklearn import preprocessing
-from sklearn import metrics
-from sklearn import datasets
-from sklearn import metrics
 
 from sklearn.base import BaseEstimator
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split
+
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+
 from sklearn.svm import SVC
 
 from sklearn.linear_model import LogisticRegression
