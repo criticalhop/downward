ff45e308c570673242e76c8c567713fd6ac90dd7
Alvaro Torralba <torralba@cs.uni-saarland.de>
2018-08-13 18:09:19 +0200

    Separated relevant rules and training

--- src/subdominization-training/gen-subdom-training.py
+++ src/subdominization-training/gen-subdom-training.py
@@ -2,6 +2,9 @@
 
 from __future__ import print_function
 
+import os
+import numpy as np
+
 from collections import defaultdict
 from rule_training_evaluator import *
 import lisp_parser
@@ -41,61 +44,68 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("runs_folder", help="path to task pddl file")
     argparser.add_argument("training_rules", type=argparse.FileType('r'), help="File that contains the rules used to generate training data by gen-subdominization-training")
-    argparser.add_argument("store_training_data", help="File to store the training data by gen-subdominization-training")    
-
-    argparser.add_argument("--debug_info", help="Include action name in the file", action="store_true")    
-
-    argparser.add_argument("--opfile", default="sas_plan", help="File to store the training data by gen-subdominization-training")    
+    argparser.add_argument("store_training_data", help="Directory to store the training data by gen-subdominization-training")    
+    argparser.add_argument("--debug-info", help="Include action name in the file", action="store_true")    
+    argparser.add_argument("--instances-relevant-rules", type=int, help="Number of instances for relevant rules", default=0)    
+    argparser.add_argument("--op-file", default="sas_plan", help="File to store the training data by gen-subdominization-training")    
+    argparser.add_argument("--num-test-instances", type=int,default=0, help="Number of instances reserved for the testing set")    
 
     options = argparser.parse_args()
 
-    training_lines = defaultdict(list)
     relevant_rules = set()
 
-    operators_filename = options.opfile
+    operators_filename = options.op_file
 
-    training_re = RuleTrainingEvaluator(options.training_rules.readlines())
-    i = 0
-    for task_run in sorted(os.listdir(options.runs_folder))[::-1]:
-        if i > 2:
-             break
-        if not os.path.isfile('{}/{}/{}'.format(options.runs_folder, task_run, operators_filename)):
-            continue
-        i += 1
-        print (i)
+    if options.instances_relevant_rules:
+        training_re = RuleTrainingEvaluator(options.training_rules.readlines())
+        i = 1
+        for task_run in sorted(os.listdir(options.runs_folder)) [::-1]:
+            if i > options.instances_relevant_rules:
+                break
+            if not os.path.isfile('{}/{}/{}'.format(options.runs_folder, task_run, operators_filename)):
+                continue
+            print (i)
+            i += 1
 
-        domain_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "domain.pddl")
-        task_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "problem.pddl")
+            domain_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "domain.pddl")
+            task_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "problem.pddl")
 
-        domain_pddl = parse_pddl_file("domain", domain_filename)
-        task_pddl = parse_pddl_file("task", task_filename)
+            domain_pddl = parse_pddl_file("domain", domain_filename)
+            task_pddl = parse_pddl_file("task", task_filename)
 
-        task = parsing_functions.parse_task(domain_pddl, task_pddl)
-        training_re.init_task(task)        
-        relaxed_reachable, atoms, actions, axioms, _ = instantiate.explore(task)
+            task = parsing_functions.parse_task(domain_pddl, task_pddl)
+            training_re.init_task(task)        
+            relaxed_reachable, atoms, actions, axioms, _ = instantiate.explore(task)
 
-        for action in actions:
-            training_re.evaluate(action)                
+            for action in actions:
+                training_re.evaluate(action)                
 
-    training_re.print_statistics()
+        training_re.print_statistics()  
 
-    
+        relevant_rules = training_re.get_relevant_rules()
 
+      
+        print ("Relevant rules: ", len(relevant_rules))
 
-    relevant_rules = training_re.get_relevant_rules()
-    print ("Relevant rules: ", len(relevant_rules))
+    if not os.path.exists(options.store_training_data):
+        os.makedirs(options.store_training_data)
 
     output_file = open('{}/relevant_rules'.format(options.store_training_data), 'w')
-    output_file.write("\n".join(relevant_rules))
+    output_file.write("\n".join(map(lambda x : x.replace('\n', ''), relevant_rules)))
     output_file.close()
 
 
-    for task_run in sorted(os.listdir(options.runs_folder)):        
-        if not os.path.isfile('{}/{}/{}'.format(options.runs_folder, task_run, operators_filename)):
-            continue
+    training_lines = defaultdict(list)
+    testing_lines = defaultdict(list)
 
+
+    all_instances = sorted([d for d in os.listdir(options.runs_folder) if os.path.isfile('{}/{}/{}'.format(options.runs_folder, d, operators_filename))])
+    np.random.seed(2018)
+    testing_instances = np.random.choice(all_instances, int(options.num_test_instances), replace=False)
+
+    for task_run in all_instances:        
         print ("Processing ", task_run)
-
+        is_test_instance = task_run in testing_instances
         domain_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "domain.pddl")
         task_filename = '{}/{}/{}'.format(options.runs_folder, task_run, "problem.pddl")
         plan_filename = '{}/{}/{}'.format(options.runs_folder, task_run, operators_filename)
@@ -122,8 +132,11 @@ if __name__ == "__main__":
                 new_line = ",".join(map (str, eval + [is_in_plan]))
                 if options.debug_info:
                     new_line = ",".join([task_run, action.name, new_line])
-                training_lines [schema].append(new_line)
 
+                if is_test_instance:
+                    testing_lines [schema].append(new_line)
+                else:
+                    training_lines [schema].append(new_line)
 
         
             
@@ -132,4 +145,10 @@ if __name__ == "__main__":
         output_file.write("\n".join(training_lines[schema]))
         output_file.close()
 
+    for schema in testing_lines:
+        output_file = open('{}/test_{}.csv'.format(options.store_training_data, schema), 'w')
+        output_file.write("\n".join(testing_lines[schema]))
+        output_file.close()
+
+                           
     #print ("Only 0/1 rules: ", len(re.get_only_0_rules()), len(re.get_only_1_rules()), len(re.get_all_rules()))
