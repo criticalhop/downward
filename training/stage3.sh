446fce43ce02fafc0b8e6584c0986b956e867e5d
Andrew Gree <andrew@hyperc.com>
2020-10-13 17:39:44 -0700

    added final training stages example scripts

---
+++ training/stage3.sh
@@ -0,0 +1,6 @@
+# matplotlib is required to run this
+
+# Below fails with K-fold needed due to classes amt, see https://stackoverflow.com/questions/45139163/roc-auc-score-only-one-class-present-in-y-true
+# python3 ../src/subdominization-training/learning/select_features.py --training-folder training_data --selector-type LOGRCV 
+
+python3 ../src/subdominization-training/learning/select_features.py --training-folder training_data --selector-type LINR
---
+++ training/stage4.sh
@@ -0,0 +1,3 @@
+# matplotlib is required to run this
+
+python3 ../src/subdominization-training/learning/train_model_for_domain.py --training-folder runs_folder --model-folder learning_m --model-type LRCV
