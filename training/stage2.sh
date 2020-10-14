2b542e8b90993fbc61963e8fc2172153f834eb9d
Andrew Gree <andrew@hyperc.com>
2020-10-13 17:38:51 -0700

    fixed stage2 to temporarily use py2.7

--- training/stage2.sh
+++ training/stage2.sh
@@ -1,1 +1,2 @@
-../src/subdominization-training/gen-subdom-training.py runs_folder ./rules_file training_data
+#!/bin/bash
+python2.7 ../src/subdominization-training/gen-subdom-training.py runs_folder ./rules_file training_data
